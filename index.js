9b1d4e6b9492b6e1c8592f3c3f0aa5b3fe1a57c9
Evelyne Colaço Garcia <84067986+EvelyneCG@users.noreply.github.com>
2021-09-24 14:27:59 +0200

    WORK DONE

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const express     = require("express");
 const bodyParser  = require("body-parser");
-const mongoose = require("mongoose");
-const uri = "mongodb+srv://secretevy:822159@cluster0.na3ue.mongodb.net/Cluster0";
-const db = mongoose.connect(uri);
 
-db.pets = require("./models/Pet.js");
-module.exports = db;
+const petRoutes = require("./routes/petRoutes");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-require("./models/Pet")(app);
-require("./routes/petRoutes")(app);
+app.use('/', petRoutes);
 
 const PORT = 5000;
 
